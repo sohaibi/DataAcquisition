b2ffa7aa88f37ebb816fea5730b025f96b621611
bennettburks <60748003+bennettburks@users.noreply.github.com>
2020-07-09 15:16:49 -0500

    added car_data functionality

--- visual/dv.py
+++ visual/dv.py
@@ -1,4 +1,5 @@
 import plotly.graph_objects as go
+import car_data
 
 figure = go.Figure()
 
@@ -9,17 +10,24 @@ data_3 = True
 data_4 = True
 data_5 = True
 
+#Function to transfer data into array
+def get_list(column_name):
+    ax = (car_data.get_var(column_name))
+    data = []
+    for itm in ax:
+        data.append(itm)
+    return data
 
 # Add traces
 if data_1 == True:
-    figure.add_trace(go.Scatter(x=[0, 2, 4, 6], y=[0, 1, 2, 3], mode='lines+markers', name='data_1'))
+    figure.add_trace(go.Scatter(x=get_list("t"), y=get_list("pos_x"), mode='lines+markers', name='data_1'))
 if data_2 == True:
-    figure.add_trace(go.Scatter(x=[0, 2, 4, 6], y=[0, 4, 5, 6], mode='lines+markers', name='data_2'))
+    figure.add_trace(go.Scatter(x=get_list("t"), y=get_list("pos_y"), mode='lines+markers', name='data_2'))
 if data_3 == True:
-    figure.add_trace(go.Scatter(x=[0, 2, 4, 6], y=[0, 7, 7.5, 8], mode='lines+markers', name='data_3'))
+    figure.add_trace(go.Scatter(x=get_list("t"), y=get_list("x"), mode='lines+markers', name='data_3'))
 if data_4 == True:
-    figure.add_trace(go.Scatter(x=[0, 2, 4, 6], y=[0, 2, 7, 9], mode='lines+markers', name='data_4'))
+    figure.add_trace(go.Scatter(x=get_list("t"), y=get_list("v"), mode='lines+markers', name='data_4'))
 if data_5 == True:
-    figure.add_trace(go.Scatter(x=[0, 2, 4, 6], y=[0, 2, 9, 11], mode='lines+markers', name='data_5'))
+    figure.add_trace(go.Scatter(x=get_list("t"), y=get_list("a_long"), mode='lines+markers', name='data_5'))
 
 figure.show()
