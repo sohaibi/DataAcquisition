0f9f4b420da2076596f0765e61d43b09cabbb60c
skyler-vestal <dinoswarleaf@gmail.com>
2020-07-11 16:54:05 -0500

    Adds interactive tabs to UI

---
+++ visual/graph_page.py
@@ -0,0 +1,4 @@
+class GraphPage:
+
+    def __init__(self, page_name = "Blank"):
+        self.page_name = page_name
--- visual/web.py
+++ visual/web.py
@@ -1,32 +1,42 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-
 from dash.dependencies import Input, Output
+from graph_page import GraphPage
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__)
+graph_list = [GraphPage("Settings Page"), GraphPage("Graph 1")]
+tab_list = [dcc.Tab(label="Settings", value="tab_0"),
+            dcc.Tab(label="Graph 1", value="tab_1")]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+def add_graph(name = None):
+    cur_num = len(graph_list)
+    name = f"Graph {cur_num}" if name is None else name
+    graph_list.append(GraphPage(name))
+    tab_list.append(dcc.Tab(label=name, value=f"tab_{cur_num}"))
+
 
 app.layout = html.Div([
-    dcc.Tabs(id='tabs-example', value='tab-1', children=[
-        dcc.Tab(label='Tab one', value='tab-1'),
-        dcc.Tab(label='Tab two', value='tab-2'),
-    ]),
-    html.Div(id='tabs-example-content')
-])
+    dcc.Tabs(id="graph-tabs", value="tab_0"),
+    html.Button("Add Graph", id="button"),
+    html.Div(id='tab-display')
+],id='app-container')
 
-@app.callback(Output('tabs-example-content', 'children'),
-              [Input('tabs-example', 'value')])
+
+@app.callback(Output('graph-tabs', 'children'),
+            [Input('button', 'n_clicks')])
+def update_dabs(n_clicks):
+    if n_clicks is not None:
+        add_graph()
+    return tab_list
+
+
+@app.callback(Output('tab-display', 'children'),
+            [Input('graph-tabs', 'value')])
 def render_content(tab):
-    if tab == 'tab-1':
-        return html.Div([
-            html.H3('Tab content 1')
-        ])
-    elif tab == 'tab-2':
-        return html.Div([
-            html.H3('Tab content 2')
-        ])
-
+    tab_num = int(tab[4])
+    return html.Div([
+        html.H3(graph_list[tab_num].page_name)
+    ])
 
 app.run_server(debug=True)
