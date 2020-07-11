798963d711981527aaa4ac142b74204bacd512cb
bennettburks <60748003+bennettburks@users.noreply.github.com>
2020-07-11 15:52:59 -0500

    Added data sets dictionary
    Co-authored-by: RJohny007 <rakeshjohny77@GMAIL.COM>

--- visual/dv.py
+++ visual/dv.py
@@ -3,31 +3,107 @@ import car_data
 
 figure = go.Figure()
 
-#Used to turn on and off graphs
-data_1 = True
-data_2 = True
-data_3 = True
-data_4 = True
-data_5 = True
+#Graph data
+data_sets = {
+'lap':True,
+'pos_x' : True,
+'pos_y' : True,
+'x' : True,
+'v' : False,
+'a_long' : False,
+'a_lat' : False,
+'cell_temp' : False,
+'cell_soc' : False,
+'rotor_temp_f' : False,
+'rotor_heat_f' : False,
+'rotor_temp_r' : False,
+'rotor_heat_r' : False,
+'FIN' : False,
+'FON' : False,
+'RIN' : False,
+'RON' : False,
+'yaw' : False,
+'pitch' : False,
+'roll' : False,
+'field_weakening' : False,	
+'frdf' : False,
+'fldf' : False,
+'rrdf' : False,
+'rldf' : False,
+'long_cf_f_inside' : False,	
+'long_cf_f_outside' : False,	
+'long_cf_r_inside' : False,
+'long_cf_r_outside' : False,	
+'lat_cf_f_inside' : False,
+'lat_cf_f_outside' : False,	
+'lat_cf_r_inside' : False,
+'lat_cf_r_outside' : False,	
+'max_lat_force_f' : False,
+'max_long_force_f' : False,	
+'max_lat_force_r' : False,
+'max_long_force_r' : False,	
+'front_lat_force' : False,
+'front_long_force' : False,	
+'rear_lat_force' : False,
+'rear_long_force' : False,	
+'max_motor_power' : False,	
+'motor_speed_limited' : False,	
+'traction_limited' : False,
+'motor_eff' : False,
+'battery_Re' : False,
+'battery_heat_removed' : False,	
+'cell_heat_w'	 : False,
+'battery_heat_w'	 : False,
+'battery_current' : False,	
+'cell_current' : False,	
+'battery_vout' : False,	
+'power_battery' : False,
+'a_long_g' : False,
+'a_lat_g' : False,
+'dW_lon' : False,
+'dW_lat_f' : False,
+'dW_lat_r' : False,
+'dW_spr_f' : False,
+'dW_geo_f' : False,
+'dW_uns_f' : False,
+'dW_spr_r' : False,
+'dW_geo_r' : False,
+'dW_uns_r' : False,
+'dW_spr_x' : False,	
+'dW_geo_x' : False,	
+'dW_uns_rx' : False,	
+'dW_uns_fx' : False,
+'weight' : False,
+'motor_torque' : False,	
+'motor_rpm' : False,
+'power_driving' : False,
+'power_motor' : False,
+'power_inverter' : False,
+'motor_heat_w' : False,
+'inv_heat_w' : False,
+'downforce' : False,
+'dragforce' : False,
+'sideforce' : False,
+'v_mph' : False,
+}
+
+listDelayValue = 100
 
 #Function to transfer data into array
 def get_list(column_name):
     ax = (car_data.get_var(column_name))
     data = []
+    listDelay = listDelayValue
     for itm in ax:
-        data.append(itm)
+        if listDelay == listDelayValue:
+            data.append(itm)
+            listDelay = 0
+        listDelay += 1
     return data
 
 # Add traces
-if data_1 == True:
-    figure.add_trace(go.Scatter(x=get_list("t"), y=get_list("pos_x"), mode='lines+markers', name='data_1'))
-if data_2 == True:
-    figure.add_trace(go.Scatter(x=get_list("t"), y=get_list("pos_y"), mode='lines+markers', name='data_2'))
-if data_3 == True:
-    figure.add_trace(go.Scatter(x=get_list("t"), y=get_list("x"), mode='lines+markers', name='data_3'))
-if data_4 == True:
-    figure.add_trace(go.Scatter(x=get_list("t"), y=get_list("v"), mode='lines+markers', name='data_4'))
-if data_5 == True:
-    figure.add_trace(go.Scatter(x=get_list("t"), y=get_list("a_long"), mode='lines+markers', name='data_5'))
+for key in data_sets:
+    if data_sets[key]:
+        figure.add_trace(go.Scatter(x=get_list("t"), y=get_list(key), mode='lines+markers', name=key))
 
 figure.show()
